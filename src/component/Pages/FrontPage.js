54617f2382f44498f1b81c2cf905f1c20299454f
lama9898 <catherine0320@daum.net>
2022-03-04 13:13:51 +0900

    signuppage

--- src/component/Pages/FrontPage.js
+++ src/component/Pages/FrontPage.js
@@ -11,22 +11,26 @@ function toHomePage(){
 const FrontPage = () => {
     return (
         <div className="front-content">
-            <div className="front-title">
-                <ul>
-                    <li>
-                        <Link to='/'>MovieShelf</Link>
-                    </li>
-                </ul>
+            <div className="front-content-header">
+                <div className="front-title">
+                    <Link to='/'>MovieShelf</Link>
+                </div>
+                <div className="front-login-btn">
+                    <Link to='/login'>LOGIN</Link>
+                </div>
             </div>
-
-            <div className="front-login-btn">
-                <ul>
-                    <li>
-                        <Link to='/login'>LOGIN</Link>
-                    </li>
-                </ul>
-                    
+            <div className="front-content-body">
+                <div className="front-content-body-info">
+                    왜
+                </div>
+                <div className="front-content-body-search">
+                    안
+                </div>
+                <div className="front-content-body-login">
+                    돼?ㅠㅠ
+                </div>
             </div>
+            
             
         </div>
     );
--- src/component/Pages/SignUpPage.js
+++ src/component/Pages/SignUpPage.js
@@ -1,10 +1,55 @@
 import React from 'react';
+import 'style/signuppage.css'
+
+function toHomePage(){
+    window.location.href="/"
+}
 
 const SignUpPage = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-            <p>SignUp</p>
+        <div className="signup-content">
+            <div className="signup-content-title" onClick={toHomePage}>
+                <h1>MovieShelf</h1>
+            </div>
+
+            <div className="signup-content-body">
+                <div className="signup-content-body-main">
+                    <div className="signup-content-body-main-title">
+                        <div className="signup-content-body-main-title-text">회원가입</div>
+                    </div>
+
+                    <div className="signup-content-body-main-info">
+                        <div className="signup-content-body-main-info-id">
+                            <input id="user_email" placeholder="이메일" type="email"/>
+                        </div>
+                        <div className="signup-content-body-main-info-pw">
+                            <input id="user_password" placeholder="비밀번호" type="password"/>
+                        </div>
+                        <div className="signup-content-body-main-info-pw">
+                            <input id="user_password" placeholder="비밀번호 확인" type="password"/>
+                        </div>
+                    </div>
+
+                    <div className="signup-content-body-main-btn">
+                        <div className="signup-content-body-main-btn-signup">
+                            <button id="signup">회원가입</button>
+                        </div>
+                    </div>
+
+                    <div className="signup-content-body-main-social">
+                        <div>소셜로그인</div>
+                        <hr></hr>
+
+                    </div>
+
+                    <div className="signup-content-body-main-login">
+                        <div className='signup-content-body-main-login-btn'>
+                            <a href='/login'>로그인</a>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
     );
 };
