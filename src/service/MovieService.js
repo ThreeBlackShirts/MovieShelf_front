5b5ad3ea9c8ab7ebb364d030edf45c529a960ba3
lama9898 <catherine0320@daum.net>
2022-04-17 18:37:32 +0900

    moviesearch try

---
+++ src/service/MovieService.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import AuthenticationService from "./AuthenticationService";
+
+const MOVIE_API_BASE_URL = "/movie"
+
+class MovieService {
+
+	search(input) {
+		let Data = {
+			input : input
+		}
+		return axios.post(MOVIE_API_BASE_URL+"/search", JSON.stringify(Data), {
+			headers: {
+				"Content-Type": 'application/json',
+			},
+		});
+	}
+
+	all() {
+		//all(movieTitle, moviePoster, movieRank) {
+			/*
+			let movieData = {
+			movieTitle: movieTitle,
+			moviePoster: moviePoster,
+			movieRank: movieRank
+		}
+			*/
+		
+		return axios.post(MOVIE_API_BASE_URL+"/alllist",{
+			headers: {
+				"Content-Type": 'application/json',
+			}
+		});
+	}
+
+}
+
+export default new MovieService()
