fac5bd26ea78f0a24dae3d7130332af3ac26a44e
Jini-Eun <julietjin94@gmail.com>
2022-04-22 14:40:47 +0900

    Fix LoginPage localStorage proplem

--- src/component/Pages/UserInfoPage.js
+++ src/component/Pages/UserInfoPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from 'component/Header/Header';
 import UserContent from 'component/UserContent';
-import 'style/mainpage.css';
+// import 'style/mainpage.css';
 
 const UserInfoPage = () => {
     return (
--- src/component/UserContent.js
+++ src/component/UserContent.js
@@ -1,6 +1,48 @@
 import React, { Component } from 'react';
+import AuthenticationService from 'service/AuthenticationService';
+import UserService from 'service/UserService';
+
 
 class UserContent extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userEmail: localStorage.getItem("authenticatedUser") || '',
+            token: localStorage.getItem("token") || '',
+            userName: '',
+            userNickname: '',
+            hasLoginFailed: false,
+        }
+        // console.log(this.state);
+        this.getUserInfo();
+        // this.handleChange = this.handleChange.bind(this)
+    }
+
+   
+    getUserInfo = () => {
+        UserService
+            .findUserByEmail(this.state.userEmail)
+            .then((response) => {
+                console.log(response.data)
+                // this.setState({
+                //     token: response.data.data
+                // });
+            }).catch(() => {
+                console.log(error.response)
+            });
+    }
+
+    render() {
+        return (
+
+            <div className="user-info">
+                <div className="user-info-content">
+                    content
+                </div>
+            </div>
+        );
+    }
 
 }
 
