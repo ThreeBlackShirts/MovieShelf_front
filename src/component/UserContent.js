81a2ad46b8ffdf9a234b78993b230e76bcefc0f6
Jini-Eun <julietjin94@gmail.com>
2022-05-01 14:44:41 +0900

    Fix UserContent.js(userName, userNickname)

--- src/component/UserContent.js
+++ src/component/UserContent.js
@@ -1,64 +1,38 @@
-import React, { Component } from 'react';
-import AuthenticationService from 'service/AuthenticationService';
+import React, { Component, useEffect, useState } from 'react';
 import UserService from 'service/UserService';
 
+const UserContent = () => {
 
-class UserContent extends Component {
-    constructor(props) {
-        super(props)
+    const [users, setUsers] = useState([]);
 
-        this.state = {
-            userEmail: localStorage.getItem("authenticatedUser") || '',
-            token: localStorage.getItem("token") || '',
-            userName: '',
-            userNickname: '',
-            hasLoginFailed: false,
-        }
-        // console.log(this.state);
-        this.getUserInfo();
-        // this.handleChange = this.handleChange.bind(this)
-    }
-
-    handleChange = (e) => {
-        this.setState(
-            {
-                [e.target.id]: e.target.value
-            }
-        )
-    }
-   
-    getUserInfo = () => {
+    useEffect(() => {
         UserService
-            .findUserByEmail(this.state.userEmail)
+            .findUserByEmail(localStorage.getItem("authenticatedUser"))
             .then((response) => {
                 console.log(response.data)
                 // this.setState({
                 //     token: response.data.data
                 // });
-                this.state.userName = response.data.userName
-                this.state.userNickname = response.data.userNickname
-                console.log(response.data.userName)
-                console.log(this.state.userName)
-                console.log(this.state.userNickname)
+                // this.state.userName = response.data.data.userName
+                // this.state.userNickname = response.data.data.userNickname
+                // console.log(response.data.data.userName)
+                // console.log(this.state.userName)
+                // console.log(this.state.userNickname)
+                setUsers(response.data.data);
             }).catch((error) => {
                 console.log(error.response)
             });
-    }
-
-    render() {
-        return (
-
-            <div className="user-info">
-                <div className="user-info-content">
-                    content
-                    <div className="userName" name="userName" id="userName" onChange={this.handleChange}>
-                        {this.state.userName}
-                    </div>
-                </div>
+    }, []);
+    return (
+        <div>
+            <div id={users.userName} key={users.userName}>
+                {users.userName}
             </div>
-        );
-    }
-
+            <div id={users.userNickame} key={users.userNickame}>
+                {users.userNickname}
+            </div>
+        </div>
+    )
 }
 
 export default UserContent;
