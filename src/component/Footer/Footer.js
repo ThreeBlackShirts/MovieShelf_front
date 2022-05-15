eaf45da4a20765158a32522ff0b62a433607e086
lama9898 <catherine0320@daum.net>
2022-05-15 15:24:45 +0900

    css/footer&header

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -5,14 +5,40 @@ const Footer = () => {
     return (
 
         <div className="footer-wrap">
-            <div className="footer-section">
-            </div>
-            <div className="footer">
-                <hr></hr>
-                <a href="/">©ThreeBlackShirts</a>
-                <hr></hr>
+            <div className='footer'>
+                <div id="footer-orginfo">
+                    <a href="https://github.com/ThreeBlackShirts">©ThreeBlackShirts</a>
+                </div>
+                <div id='footer-sub-wrap'>
+                    <div className='footer-sub'>
+                        <div className='footer-sub-title'>소개</div>
+                        <ul className='footer-sub-list'>
+                            <li><a href="https://github.com/ThreeBlackShirts">MovieShelf</a></li>
+                            <li><a href="https://github.com/JiniEun">JiniEun</a></li>
+                            <li><a href="https://github.com/SShinMJ">SShinMJ</a></li>
+                            <li><a href="https://github.com/lama9898">lama9898</a></li>
+                        </ul>
+                    </div>
+                    <div className='footer-sub'>
+                        <div className='footer-sub-title'>유용한 링크</div>
+                        <ul className='footer-sub-list'>
+                            <li><a href="https://movie.naver.com/">네이버 영화</a></li>
+                            <li><a href="https://movie.daum.net/main">다음 영화</a></li>
+                            <li><a href="https://www.themoviedb.org/">TMDB</a></li>
+                            <li><a href="https://www.imdb.com/?ref_=nv_home">IMDb</a></li>
+                        </ul>
+                    </div>
+                    <div className='footer-sub'>
+                        <div className='footer-sub-title'>지원</div>
+                        <ul className='footer-sub-list'>
+                            <li>문의</li>
+                        </ul>
+                    </div>
+                </div>
+                
             </div>
         </div>
+
     );
 };
 
