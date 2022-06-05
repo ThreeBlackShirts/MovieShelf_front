71a87ddf4a25b3a949e947e9c8ee11ba57403df5
lama9898 <catherine0320@daum.net>
2022-06-05 18:03:39 +0900

    react/userinfopage

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -6,39 +6,21 @@ const Footer = () => {
 
         <div className="footer-wrap">
             <div className='footer'>
-                <div id="footer-orginfo">
-                    <a href="https://github.com/ThreeBlackShirts">©ThreeBlackShirts</a>
+                <div className="footer-orginfo">
+                    <a className="footer-links" href="https://github.com/ThreeBlackShirts">©ThreeBlackShirts</a>
                 </div>
-                <div id='footer-sub-wrap'>
-                    <div className='footer-sub'>
-                        <div className='footer-sub-title'>소개</div>
-                        <ul className='footer-sub-list'>
-                            <li><a href="https://github.com/ThreeBlackShirts">MovieShelf</a></li>
-                            <li><a href="https://github.com/JiniEun">JiniEun</a></li>
-                            <li><a href="https://github.com/SShinMJ">SShinMJ</a></li>
-                            <li><a href="https://github.com/lama9898">lama9898</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer-sub'>
-                        <div className='footer-sub-title'>유용한 링크</div>
-                        <ul className='footer-sub-list'>
-                            <li><a href="https://movie.naver.com/">네이버 영화</a></li>
-                            <li><a href="https://movie.daum.net/main">다음 영화</a></li>
-                            <li><a href="https://www.themoviedb.org/">TMDB</a></li>
-                            <li><a href="https://www.imdb.com/?ref_=nv_home">IMDb</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer-sub'>
-                        <div className='footer-sub-title'>지원</div>
-                        <ul className='footer-sub-list'>
-                            <li>문의</li>
-                        </ul>
-                    </div>
+                <div className='footer-about'>
+                    <a className="footer-links" href="https://github.com/JiniEun">JiniEun</a>
+                    &nbsp; | &nbsp;
+                    <a className="footer-links" href="https://github.com/SShinMJ">SShinMJ</a>
+                    &nbsp; | &nbsp;
+                    <a className="footer-links" href="https://github.com/lama9898">lama9898</a>
+                    &nbsp; | &nbsp;
+                    <a className="footer-links" >문의</a>
                 </div>
                 
             </div>
         </div>
-
     );
 };
 
--- src/component/Pages/UserInfoPage.js
+++ src/component/Pages/UserInfoPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Footer from '../Footer/Footer';
 import Header from 'component/Header/Header';
 import UserContent from 'component/UserContent';
-// import 'style/mainpage.css';
+import 'style/userinfopage.css';
 
 const UserInfoPage = () => {
     return (
-        <div>
+        <div className='userinfopage'>
             <Header />
             <UserContent />
             <Footer />
