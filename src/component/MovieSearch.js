8f4a6d8dd254fd6ab9ac52fe4e5c5a3f7235f2cc
SShinMJ <winny0678@gmail.com>
2022-06-24 20:57:54 +0900

    Fix Movie Detail

--- src/component/MovieSearch.js
+++ src/component/MovieSearch.js
@@ -3,10 +3,15 @@ import PropTypes from "prop-types"
 
 import 'style/listpage.css';
 
+function setLocation(title) {
+    localStorage.setItem('target', title)
+    location.href = '/detail'
+}
+
 function Movie({title, poster}) {
     return (
         <div className="listpage-content-result-item">
-            <a href="/detail/{title}">
+            <a onClick={() => setLocation(title)}>
                 <div className="listpage-content-result-item-pic"><img src={poster}/></div>
                 <div className="listpage-content-result-item-info">{title}</div>
             </a>
