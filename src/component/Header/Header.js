4e279e0b92eb8ab66145cf39e7827c84ef7c2cd5
Jin <julietjin94@gmail.com>
2022-08-21 04:29:57 +0900

    fix : kakao social login function

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,93 +1,106 @@
 import React from 'react';
 import AuthenticationService from 'service/AuthenticationService';
 import { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-import 'style/header.css'
+import 'style/header.css';
 
-import { FiSearch } from "react-icons/fi";
-import { ImBooks } from "react-icons/im";
-import { IoIosSettings } from "react-icons/io";
+import { FiSearch } from 'react-icons/fi';
+import { ImBooks } from 'react-icons/im';
+import { IoIosSettings } from 'react-icons/io';
 
 const Header = () => {
-    let navigate = useNavigate();
-    const [onLogin, setOnLogin] = useState(AuthenticationService.isUserLoggedIn);
+	let navigate = useNavigate();
+	const [onLogin, setOnLogin] = useState(AuthenticationService.isUserLoggedIn);
 
-    function toHomePage(){
-        navigate("/main")
-    }
-    
-    function toGenreListPage(){
-        navigate("/genre")
-    }
-    
-    function toUserInfoPage(){
-        navigate("/userinfo")
-    }
+	function toHomePage() {
+		navigate('/main');
+	}
 
-    function onKeyPress(e){
-        if(e.key == 'Enter')
-            search()
-    }
-    
-    function toUserSettingPage(){
-        navigate("/usersetting")
-    }
-    
-    function doLogout(){
-        AuthenticationService.logout()
-        alert("로그아웃 되었습니다.")
-        setOnLogin(false);
-    }
-    
-    function doLogin(){
-        navigate("/login")
-    }
-    
-    function search() {
-        const input = document.getElementById("search-input").value
-        if(input !== null && input !== ""){
-            location.href = `/list/?search=${input}`
-        }else
-            alert("검색어를 입력해주세요.")
-    }
+	function toGenreListPage() {
+		navigate('/genre');
+	}
 
-    return (
-        <div className="header-container">
-            <div className="header-content-title">
-                <div className="header-content-title-logo">
-                    <h1 className="header-content-title-logo-h1" onClick={toHomePage}>MovieShelf</h1>
-                </div>
-                <div className="header-content-title-menu">
-                    <div className="header-content-title-top">
-                        <h4 className="header-content-title-item" onClick={toHomePage}>Top20</h4>
-                    </div>
-                    <div className="header-content-title-genre">
-                        <h4 className="header-content-title-item" onClick={toGenreListPage}>Genre</h4>
-                    </div>
-                </div>
-            </div>
+	function toUserInfoPage() {
+		navigate('/userinfo');
+	}
 
-            <div className="header-content-userbtn">
-                <div className="header-content-userbtn-search-btn">
-                    <input type="text" className="header-content-userbtn-search-btn-text"  id='search-input' placeholder="영화 제목을 입력해주세요" onKeyUp={onKeyPress}></input>
-                    <FiSearch className="header-content-userbtn-search-btn-icon" onClick={search}/>
-                </div>
-                <div className="header-content-userbtn-myshelf">
-                    <ImBooks className="header-content-userbtn-myshelf-icon" onClick={toUserInfoPage}/>
-                </div>
-                <div className="header-content-userbtn-setting">
-                    <IoIosSettings className="header-content-userbtn-setting-icon" onClick={toUserSettingPage}/>
-                </div>
-                {onLogin && <div className="header-content-userbtn-login">
-                    <h4 className="header-content-userbtn-login-btn" onClick={doLogout}>LOGOUT</h4>
-                </div>}
-                {!onLogin && <div className="header-content-userbtn-login">
-                    <h4 className="header-content-userbtn-login-btn" onClick={doLogin}>LOGIN</h4>
-                </div>}
-            </div>
-        </div>
-    );
+	function onKeyPress(e) {
+		if (e.key == 'Enter') search();
+	}
+
+	function toUserSettingPage() {
+		navigate('/usersetting');
+	}
+
+	function doLogout() {
+		AuthenticationService.logout();
+		alert('로그아웃 되었습니다.');
+		setOnLogin(false);
+		navigate('/main');
+	}
+
+	function doLogin() {
+		navigate('/login');
+	}
+
+	function search() {
+		const input = document.getElementById('search-input').value;
+		if (input !== null && input !== '') {
+			location.href = `/list/?search=${input}`;
+		} else alert('검색어를 입력해주세요.');
+	}
+
+	return (
+		<div className="header-container">
+			<div className="header-content-title">
+				<div className="header-content-title-logo">
+					<h1 className="header-content-title-logo-h1" onClick={toHomePage}>
+						MovieShelf
+					</h1>
+				</div>
+				<div className="header-content-title-menu">
+					<div className="header-content-title-top">
+						<h4 className="header-content-title-item" onClick={toHomePage}>
+							Top20
+						</h4>
+					</div>
+					<div className="header-content-title-genre">
+						<h4 className="header-content-title-item" onClick={toGenreListPage}>
+							Genre
+						</h4>
+					</div>
+				</div>
+			</div>
+
+			<div className="header-content-userbtn">
+				<div className="header-content-userbtn-search-btn">
+					<input type="text" className="header-content-userbtn-search-btn-text" id="search-input" placeholder="영화 제목을 입력해주세요" onKeyUp={onKeyPress}></input>
+					<FiSearch className="header-content-userbtn-search-btn-icon" onClick={search} />
+				</div>
+				<div className="header-content-userbtn-myshelf">
+					<ImBooks className="header-content-userbtn-myshelf-icon" onClick={toUserInfoPage} />
+				</div>
+				<div className="header-content-userbtn-setting">
+					<IoIosSettings className="header-content-userbtn-setting-icon" onClick={toUserSettingPage} />
+				</div>
+				{onLogin && (
+					<div className="header-content-userbtn-login">
+						<h4 className="header-content-userbtn-login-btn" onClick={doLogout}>
+							LOGOUT
+						</h4>
+					</div>
+				)}
+				{!onLogin && (
+					<div className="header-content-userbtn-login">
+						<h4 className="header-content-userbtn-login-btn" onClick={doLogin}>
+							LOGIN
+						</h4>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- src/component/Pages/LoginPage.js
+++ src/component/Pages/LoginPage.js
@@ -1,120 +1,119 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import AuthenticationService from 'service/AuthenticationService';
 
 import 'style/loginpage.css';
 
 const LoginPage = () => {
-    let navigate = useNavigate();
+	let navigate = useNavigate();
 
-    function toHomePage() { 
-        navigate("/")   
-    }
+	function toHomePage() {
+		navigate('/');
+	}
 
-    function toBack() { 
-        navigate(-1)   
-    }
+	function toBack() {
+		navigate(-1);
+	}
 
-    const [userEmail, setUserEmail] = useState(localStorage.getItem("authenticatedUser") || '');
-    const [userPassword, setUserPassword] = useState('');
-    const [token, setToken] = useState(localStorage.getItem("token") || '');
-    const [hasLoginFailed, setHasLoginFailed] = useState(false);
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+	const [userEmail, setUserEmail] = useState(localStorage.getItem('authenticatedUser') || '');
+	const [userPassword, setUserPassword] = useState('');
+	const [token, setToken] = useState(localStorage.getItem('token') || '');
+	const [hasLoginFailed, setHasLoginFailed] = useState(false);
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-    const onChangeEmail = e => setUserEmail(e.target.value);
-    const onChangePassword = e => setUserPassword(e.target.value);
+	const onChangeEmail = (e) => setUserEmail(e.target.value);
+	const onChangePassword = (e) => setUserPassword(e.target.value);
 
+	function loginClicked() {
+		AuthenticationService.login(userEmail, userPassword)
+			.then((response) => {
+				console.log('loginClicked');
+				setToken(response.data.data);
 
-    function loginClicked() {
-        AuthenticationService
-            .login(userEmail, userPassword)
-            .then((response) => {
-                console.log('loginClicked');
-                setToken(response.data.data)
+				AuthenticationService.registerSuccessfulLoginForJwt(userEmail, response.data.data);
+				setShowSuccessMessage(true);
+				setHasLoginFailed(false);
 
-                AuthenticationService.registerSuccessfulLoginForJwt(userEmail, response.data.data);
-                setShowSuccessMessage(true)
-                setHasLoginFailed(false)
+				//현재 위치가 "/" 였으면, "/main"으로, 아니라면, -1
+				toBack();
+			})
+			.catch(() => {
+				setShowSuccessMessage(false);
+				setHasLoginFailed(true);
+				alert('Login Failed');
+			});
+	}
 
-                //현재 위치가 "/" 였으면, "/main"으로, 아니라면, -1
-                toBack()
-            }).catch(() => {
-                setShowSuccessMessage(false)
-                setHasLoginFailed(true)
-                alert('Login Failed');
-            });
-    }
+	function socialLoginGoogle() {
+		console.log('google login clicked');
+		AuthenticationService.loginSocialGoogle();
+	}
+	function socialLoginKakao() {
+		console.log('kakao login clicked');
+		AuthenticationService.loginSocialKakao();
+	}
 
-    function socialLoginGoogle() {
-        console.log("google login clicked")
-        AuthenticationService.loginSocialGoogle();
-    }
-    function socialLoginKakao() {
-        console.log("kakao login clicked")
-        AuthenticationService.loginSocialKakao();
-    }
+	return (
+		<div className="login-content">
+			<div className="login-content-title">
+				<h1 id="login-content-title-logo" onClick={toHomePage}>
+					MovieShelf
+				</h1>
+			</div>
 
-    return (
-        <div className="login-content">
-            <div className="login-content-title">
-                <h1 id='login-content-title-logo' onClick={toHomePage}>MovieShelf</h1>
-            </div>
+			<div className="login-content-body">
+				<div className="login-content-body-main">
+					{hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
+					{showSuccessMessage && <div>Login Sucessful</div>}
+					<div className="login-content-body-main-title">
+						<div className="login-content-body-main-title-text">로그인</div>
+					</div>
+					<form>
+						<div className="login-content-body-main-info">
+							<div className="login-content-body-main-info-id">
+								<input id="userEmail" name="userEmail" placeholder="이메일" type="email" onChange={onChangeEmail} />
+							</div>
+							<div className="login-content-body-main-info-pw">
+								<input id="userPassword" name="userPassword" placeholder="비밀번호" type="password" autoComplete="on" onChange={onChangePassword} />
+							</div>
+						</div>
+					</form>
 
-            <div className="login-content-body">
-                <div className="login-content-body-main">
-                    {hasLoginFailed && <div className="alert alert-warning">Invalid Credentials</div>}
-                    {showSuccessMessage && <div>Login Sucessful</div>}
-                    <div className="login-content-body-main-title">
-                        <div className="login-content-body-main-title-text">로그인</div>
-                    </div>
-                    <form >
-                    <div className="login-content-body-main-info">
-                        <div className="login-content-body-main-info-id">
-                                <input id="userEmail" name="userEmail" placeholder="이메일" type="email" onChange={onChangeEmail}/>
-                        </div>
-                        <div className="login-content-body-main-info-pw">
-                                <input id="userPassword" name="userPassword" placeholder="비밀번호" type="password" autoComplete="on" onChange={onChangePassword} />
-                        </div>
-                        </div>
-                    </form>
+					<div className="login-content-body-main-btn">
+						<div className="login-content-body-main-btn-rememberme">
+							<form className="login-content-body-main-btn-rememberme-form">
+								<input type="checkbox" id="rememberme_btn" /> <label htmlFor="rememberme_btn">이메일 기억하기</label>
+							</form>
+						</div>
 
-                    <div className="login-content-body-main-btn">
-                        <div className="login-content-body-main-btn-rememberme">
-                            <form className="login-content-body-main-btn-rememberme-form">
-                                <input type="checkbox" id='rememberme_btn' /> <label htmlFor='rememberme_btn'>이메일 기억하기</label>
-                            </form>
-                        </div>
+						<div className="login-content-body-main-btn-login">
+							<button id="login" onClick={loginClicked}>
+								나의 책장 보러가기
+							</button>
+						</div>
+					</div>
 
-                        <div className="login-content-body-main-btn-login">
-                            <button id="login" onClick={loginClicked}>나의 책장 보러가기</button>
-                        </div>
-                        </div>
-                    
+					<div className="login-content-body-main-social">
+						<div>소셜로그인</div>
+						<hr></hr>
+						<div className="login-content-body-main-social-btn">
+							<img className="social-login-btn" src={require('../../images/button/btn_google_signin_light_normal_web.png')} onClick={socialLoginGoogle} />
+						</div>
+						<div className="login-content-body-main-social-btn">
+							<img className="social-login-btn" src={require('../../images/button/kakao_login_medium_narrow.png')} onClick={socialLoginKakao} />
+						</div>
+					</div>
 
-                    <div className="login-content-body-main-social">
-                        <div>소셜로그인</div>
-                        <hr></hr>
-                        <div className='login-content-body-main-social-btn'>
-                            <img className='social-login-btn' src={require('../../images/button/btn_google_signin_light_normal_web.png')} onClick={socialLoginGoogle} />
-                        </div>
-                        <div className='login-content-body-main-social-btn'>
-                            <img className='social-login-btn' src={require('../../images/button/kakao_login_medium_narrow.png')} onClick={socialLoginKakao} />
-                        </div>
-                    </div>
-
-                    <div className="login-content-body-main-signup">
-                        <div className='login-content-body-main-signup-intro'>
-                            아직 나만의 책장이 없으신가요?
-                        </div>
-                        <div className='login-content-body-main-signup-btn'>
-                            <Link to='/signup'>SIGN UP</Link>
-                        </div>
-                    </div>
-                
-                </div>
-            </div>
-        </div>
-    )
-}
+					<div className="login-content-body-main-signup">
+						<div className="login-content-body-main-signup-intro">아직 나만의 책장이 없으신가요?</div>
+						<div className="login-content-body-main-signup-btn">
+							<Link to="/signup">SIGN UP</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default LoginPage;
